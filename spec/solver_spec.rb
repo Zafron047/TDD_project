205e5a6bb12374600352134343853ddddae2b9fc
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-08 15:11:09 +0600

    Add Fizzbuzz Testcases

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -30,4 +30,19 @@ describe Solver do
       expect(solver.reverse('THrOW')).to eq('worht')
     end
   end
+
+  describe 'Fizzbuzz Test' do
+    it 'should return fizzbuzz if given argument is divisible by 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    it 'should return fizz if given argument is divisible by 3' do
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+    it 'should return buzz if given argument is divisible by 5' do
+      expect(solver.fizzbuzz(25)).to eq('buzz')
+    end
+    it 'should return integer argument if given argument is not divisible by 3 or 5' do
+      expect(solver.fizzbuzz(7)).to eq(7)
+    end
+  end
 end
